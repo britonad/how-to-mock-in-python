24e3e142b2ffe2fc7fecb9faafb7e3eb530e9cdb
Vladyslav Krylasov <vladyslav.krylasov@gmail.com>
2020-03-12 17:01:24 +0000

    ✅ Add mocks

---
+++ make_mock_by_function_import.py
@@ -0,0 +1,13 @@
+from unittest.mock import patch
+from core.utils import list_dirs_only
+
+
+patcher = patch('__main__.list_dirs_only')
+original = list_dirs_only
+mock = patcher.start()
+
+print(list_dirs_only)
+print(mock)
+assert list_dirs_only == mock
+
+patcher.stop()
---
+++ make_mock_by_module_import.py
@@ -0,0 +1,13 @@
+from unittest.mock import patch
+from core import utils
+
+
+patcher = patch('core.utils.list_files_only')
+original = utils.list_files_only
+mock = patcher.start()
+
+print(utils.list_files_only)
+print(mock)
+assert utils.list_files_only == mock
+
+patcher.stop()
