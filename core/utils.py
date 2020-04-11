fd9db4f16110146494ae2d40fb6b5cd428a7e519
Vladyslav Krylasov <vladyslav.krylasov@gmail.com>
2020-04-11 16:17:36 +0100

    ✨ Add a dummy class to utils

--- core/utils.py
+++ core/utils.py
@@ -11,3 +11,13 @@ def list_dirs_only() -> list:
     """Lists directories in the current path."""
 
     return [p for p in pathlib.Path('.').iterdir() if p.is_dir()]
+
+
+class PythonFilesLookUp:
+    def __init__(self, path):
+        self.path = path
+
+    def list_python_files(self) -> list:
+        """Lists python files by a provided path."""
+
+        return [p for p in pathlib.Path(self.path).glob('*.py')]
